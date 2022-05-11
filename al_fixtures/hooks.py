46830546b3c5a4a524b3fe656a1587ae76e94ca3
S-Amine <saidi.amine.p@gmail.com>
2022-05-11 16:04:42 +0100

    debug import (hope this is last debu)g

--- al_fixtures/hooks.py
+++ al_fixtures/hooks.py
@@ -15,14 +15,14 @@ app_include_js = "/assets/al_fixtures/js/custom_number_format.js"
 
 # import frappe function & custom function for customisation
 import frappe as _frappe
-import al_fixtures as _al_fixtures
+import al_fixtures.utils.data as _al_fixtures
 
 # Replace frappe function with custom function
-_frappe.utils.data.fmt_money = _al_fixtures.utils.data.custom_fmt_money
-_frappe.utils.fmt_money = _al_fixtures.utils.data.custom_fmt_money
+_frappe.utils.data.fmt_money = _al_fixtures.custom_fmt_money
+_frappe.utils.fmt_money = _al_fixtures.custom_fmt_money
 
-_frappe.utils.data.money_in_words = _al_fixtures.utils.data.money_in_words
-_frappe.utils.money_in_words = _al_fixtures.utils.data.money_in_words
+_frappe.utils.data.money_in_words = _al_fixtures.money_in_words
+_frappe.utils.money_in_words = _al_fixtures.money_in_words
 
 
 # override_doctype_class = {
